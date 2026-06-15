3c8f981595b257f0f7cd140c3aa6d7e9bd5947cb
Unmesh Kakuste <148318094+UnmeshKakuste@users.noreply.github.com>
2026-06-15 17:55:13 +0530

    Add files via upload

---
+++ Assignment_2/Assign2_Q9.py
@@ -0,0 +1,8 @@
+# print("Enter your name : ")
+name = input("Enter your name: ")
+
+# print("Enter your age : ")
+age = int(input("Enter your age: "))
+
+print("Hello", name, ", you will turn", age+1, "next year.")
+
