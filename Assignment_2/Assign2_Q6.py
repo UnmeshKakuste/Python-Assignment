f47b70ca13e2062c6fec724b40369cb3dbb0da23
Unmesh Kakuste <148318094+UnmeshKakuste@users.noreply.github.com>
2026-06-15 17:13:29 +0530

    Add files via upload

---
+++ Assignment_2/Assign2_Q6.py
@@ -0,0 +1,13 @@
+print("Enter First number: ")
+No1 = int(input())
+
+print("Enter Second number: ")
+No2 = int(input())
+
+print("Addition : ", No1 + No2)
+
+print("Subtraction : ", No1 - No2)
+
+print("Multiplication : ", No1 * No2)
+
+print("Divison : ", No1 / No2)
