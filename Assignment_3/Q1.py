3d279710a54659f36281c328afe9e0575ede041d
Unmesh Kakuste <148318094+UnmeshKakuste@users.noreply.github.com>
2026-06-22 22:41:29 +0530

    Add files via upload

---
+++ Assignment_3/Q1.py
@@ -0,0 +1,16 @@
+def Multiplication(No1,No2):
+    return No1 * No2
+
+def main():
+    print("Enter first number : ")
+    Value1 = int(input())
+
+    print("Enter second number : ")
+    Value2 = int(input())
+
+    Ret = Value1 * Value2
+
+    print("Multiplication is : ",Ret)
+    
+if __name__ == "__main__":
+    main()
---
+++ Assignment_3/Q6.py
@@ -0,0 +1,5 @@
+from sys import getsizeof
+data = 4
+print("Data type:", type(data))
+print("Memory address:", id(data))
+print("Size in bytes:", getsizeof(data))
